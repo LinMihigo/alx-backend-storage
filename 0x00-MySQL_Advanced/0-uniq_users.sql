3925ee81121a9d7c946852301e29f88638a4729c
LinMihigo <linmihigo@gmail.com>
2025-05-02 11:07:01 +0200

    Task 0 to 2

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Create holberton database if it doesn't exist.
+-- Creates the table - users if it doesn't exist.
+CREATE DATABASE IF NOT EXISTS holberton;
+USE holberton;
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+)
---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- Creates a table - users
+CREATE DATABASE IF NOT EXISTS holberton;
+USE holberton;
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
