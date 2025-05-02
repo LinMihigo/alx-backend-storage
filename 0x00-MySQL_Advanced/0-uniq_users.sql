d709612fba7843c501b2f2dbc31a5d7a0fde663d
LinMihigo <linmihigo@gmail.com>
2025-05-02 11:11:47 +0200

    Task 0 - 1st fix

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,6 +2,7 @@
 -- Creates the table - users if it doesn't exist.
 CREATE DATABASE IF NOT EXISTS holberton;
 USE holberton;
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
