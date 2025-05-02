1b3064e338c981ab2c3e965fad07e811da445eb9
LinMihigo <linmihigo@gmail.com>
2025-05-02 11:25:37 +0200

    Task 0 - 2nd exist

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,5 @@
 -- Create holberton database if it doesn't exist.
 -- Creates the table - users if it doesn't exist.
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
