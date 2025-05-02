172752d89270e183f6844407cbbdd4e0bbff0e28
LinMihigo <linmihigo@gmail.com>
2025-05-02 12:31:57 +0200

    Task 3 - 2nd fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,10 +2,10 @@
 -- longevity
 SELECT
     band_name,
-    (COALESCE(split, 2022) - formed) AS lifespan
+    (IFNULL(split, 2022) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
-    style = '%Glam rock%'
+    FIND_IN_SET('Glam rock', IFNULL(style, '')) > 0
 ORDER BY
     lifespan DESC;
