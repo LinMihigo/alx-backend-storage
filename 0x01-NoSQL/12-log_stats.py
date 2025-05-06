c8ab0394edef8c99e004dd2d5cdd1fcdcf9b9e1c
LinMihigo <linmihigo@gmail.com>
2025-05-06 22:41:06 +0200

    Revert "Task 12 - first fix"
    This reverts commit 291d5ee93be30dabb1d38a0af1405a89926bacda.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,17 +4,17 @@
 from pymongo import MongoClient
 
 
-def print_nginx_request_logs(nginx_collection):
+def printer(nginx_collector):
     """Prints stats about Nginx requests logs
     """
-    print('{} logs'.format(nginx_collection.count_documents({})))
+    print('{} logs'.format(nginx_collector.count_documents({})))
     print('Methods:')
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
-        req_count = len(list(nginx_collection.find({'method': method})))
+        req_count = len(list(nginx_collector.find({'method': method})))
         print('\tmethod {}: {}'.format(method, req_count))
     status_checks_count = len(list(
-        nginx_collection.find({'method': 'GET', 'path': '/status'})
+        nginx_collector.find({'method': 'GET', 'path': '/status'})
     ))
     print('{} status check'.format(status_checks_count))
 
@@ -23,7 +23,7 @@ def run():
     """provides some stats about Nginx logs stored in MongoDB
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    print_nginx_request_logs(client.logs.nginx)
+    printer(client.logs.nginx)
 
 
 if __name__ == '__main__':
